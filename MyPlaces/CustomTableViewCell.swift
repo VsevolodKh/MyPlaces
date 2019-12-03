9f5ad289ddb844a410b0e38f0ce2c48e11f75ba0
VsevolodKh <vsevolodkhodyrev@gmail.com>
2019-12-03 09:07:28 +0400

    New class "CustomViewCell" Some changes in MainViewController class

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Всеволод on 03.12.2019.
+//  Copyright © 2019 Vsevolod Khodyrev. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+  
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLable: UILabel!
+    @IBOutlet var locationLable: UILabel!
+    @IBOutlet var typeLable: UILabel!
+    
+}
